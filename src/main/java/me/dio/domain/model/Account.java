a16e4423d4b6d6098d4d082ca71697390e96947e
Tavaressan <vitortctavares@gmail.com>
2025-07-22 13:32:18 -0300

    feat: Implementação da camada de domínio

---
+++ src/main/java/me/dio/domain/model/Account.java
@@ -0,0 +1,33 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity(name = "tb_account")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String number;
+
+    private String agency;
+
+    @Column(scale = 12 ,precision = 2)
+    private BigDecimal balance;
+
+    @Column(name = "additional_limit", scale = 12 ,precision = 2)
+    private BigDecimal limit;
+
+}
---
+++ src/main/java/me/dio/domain/model/Card.java
@@ -0,0 +1,28 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity(name = "tb_card")
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(scale = 12 ,precision = 2)
+    private BigDecimal number;
+
+    @Column(name = "available_limit", scale = 12 ,precision = 2)
+    private BigDecimal limit;
+
+}
